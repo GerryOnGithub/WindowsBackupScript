2c265427bd8ccd93709f2a6062b921538aa816af
GerryOnGithub <23443806+GerryOnGithub@users.noreply.github.com>
2024-08-24 11:36:17 -0700

    Create RunBackup.bat

---
+++ RunBackup.bat
@@ -0,0 +1,14 @@
+rem Modify this script as you like, then run in powershell
+rem Below copy Documents to the Q drive (which is a thumbnail drive in this case)
+
+@echo off
+setlocal EnableDelayedExpansion
+
+for /f %%I in ('powershell -command "Get-Date -Format yyyy-MM-dd"') do set datestamp=%%I
+
+rem add as many lines like the following as you need
+rem /XD is used to exclude folders
+
+robocopy "C:\Users\Gerry\Documents" "Q:\Backups\Asus_%datestamp%\Documents" /E /R:0 /W:0 /MT /XO /XD "C:\Users\Gerry\Documents\My Videos" "C:\Users\Gerry\Documents\My Pictures" "C:\Users\Gerry\Documents\My Music"
+
+endlocal
